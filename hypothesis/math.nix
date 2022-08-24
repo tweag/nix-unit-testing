03f090eb1deec46f215ab2b26b59f91cc6a27211
Daniel Baker <daniel.baker@tweag.io>
2022-08-24 14:14:00 -0700

    updated comment about factorial function again.

--- hypothesis/math.nix
+++ hypothesis/math.nix
@@ -5,7 +5,7 @@ in rec {
   # Returns the factorial of a non-negative integer.
   factorial = x:
     if (x < 0) || !(builtins.isInt x)
-    then throw "factorial only takes positive integers, got x = ${toString x}"
+    then throw "factorial only takes non-negative integers, got x = ${toString x}"
     else if x == 0
     then 1
     else x * factorial (x - 1);
